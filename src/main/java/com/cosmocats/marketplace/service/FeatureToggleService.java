77570631e5b861f984d5fe4446433a2eb358021b
senatede <bhupalo@kse.org.ua>
2025-12-19 01:13:51 +0200

    fix: feature flags from configuration

--- src/main/java/com/cosmocats/marketplace/service/FeatureToggleService.java
+++ src/main/java/com/cosmocats/marketplace/service/FeatureToggleService.java
@@ -1,27 +1,18 @@
 package com.cosmocats.marketplace.service;
 
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.core.env.Environment;
 
 @Component
 public class FeatureToggleService {
 
-    private final boolean cosmoCatsEnabled;
-    private final boolean kittyProductsEnabled;
+    private final Environment env;
 
-    public FeatureToggleService(
-            @Value("${feature.cosmoCats.enabled:false}") boolean cosmoCatsEnabled,
-            @Value("${feature.kittyProducts.enabled:false}") boolean kittyProductsEnabled
-    ) {
-        this.cosmoCatsEnabled = cosmoCatsEnabled;
-        this.kittyProductsEnabled = kittyProductsEnabled;
+    public FeatureToggleService(Environment env) {
+        this.env = env;
     }
 
     public boolean isEnabled(String featureName) {
-        return switch (featureName) {
-            case "cosmoCats" -> cosmoCatsEnabled;
-            case "kittyProducts" -> kittyProductsEnabled;
-            default -> false;
-        };
+        return Boolean.parseBoolean(env.getProperty("feature." + featureName + ".enabled", "false"));
     }
 }
